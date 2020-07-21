9ef1dd8e058fb32c5b4c1e4a211e58e61026430b
fabio Gonzalez <fabiomgs12@gmail.com>
2020-07-20 21:35:27 -0500

    docs

---
+++ docs/precache-manifest.4a9bb2ae29b1734d4344e1b54ee408c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95f67273f67e952ec2dcd35b09f3ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b0ae3e881a1f7fa92a",
+    "url": "/static/css/main.7d0fc4da.chunk.css"
+  },
+  {
+    "revision": "7722d85366ffb1b3bc75",
+    "url": "/static/js/2.590da7d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590da7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b0ae3e881a1f7fa92a",
+    "url": "/static/js/main.3253fb2b.chunk.js"
+  },
+  {
+    "revision": "2b40892910ff1dc18423",
+    "url": "/static/js/runtime-main.1af7f805.js"
+  }
+]);
